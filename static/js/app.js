981b4db29c747b0d4e5f3927dc118aa4399f6627
Fernando Jara <jarafer96@gmail.com>
2026-03-02 03:36:48 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2107,6 +2107,7 @@ function irAContacto() {
 window.irAContacto = irAContacto; 
     
 function renderProducto(p, esLCP = false) {
+  alert("Renderizando producto: " + p.nombre);  
   const card = document.createElement("div");
   card.className = "col-lg-4 col-md-6 col-sm-12 mb-4 fade-reorder card-producto";
   card.dataset.id = p.id_base;
@@ -2408,6 +2409,7 @@ function renderProducto(p, esLCP = false) {
   requestAnimationFrame(() => card.classList.add("show"));
   setTimeout(() => card.classList.remove("fade-reorder"), 50);
 
+  alert("Producto renderizado: " + p.nombre);  
   return card;
 }
 
