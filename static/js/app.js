aaa540cbd9e3cc8ddf5f84dea50fb615919613c4
Fernando Jara <jarafer96@gmail.com>
2026-03-07 17:41:26 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2717,11 +2717,11 @@ setTimeout(() => {
     // Ocultar al hacer scroll
     panelGrupos.addEventListener('scroll', () => {
       splash.style.opacity = '0';
-      setTimeout(() => splash.remove(), 500);
+      setTimeout(() => splash.remove(), 1000);
     }, { once: true });
 
     // También ocultar después de 5 segundos (opcional, puedes mantener la animación)
   } else {
     splash.remove();
   }
-}, 1000); // Esperar 1 segundo para que los grupos se hayan cargado
+}, 2300); // Esperar 1 segundo para que los grupos se hayan cargado
