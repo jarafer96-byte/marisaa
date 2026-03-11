9abc73e5d5b185abe606bb371c19e4e767ea058b
Fernando Jara <jarafer96@gmail.com>
2026-03-11 00:46:12 -0300

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1646,7 +1646,7 @@ function renderPagina(pagina, productos) {
   const cont = document.getElementById("productos");
   if (!cont) return;
 
-  const itemsPorPagina = window.innerWidth <= 767 ? 5 : 12; // o usa getItemsPorPagina()
+  const itemsPorPagina = getItemsPorPagina();
 
   // Si no hay productos (null, undefined o array vacío), mostramos skeletons
   if (!productos || productos.length === 0) {
