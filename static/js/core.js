031f6ac38bcf31e51eadf93a7ead14c808a7a202
Fernando Jara <jarafer96@gmail.com>
2026-03-13 22:25:38 -0300

    Update core.js

--- static/js/core.js
+++ static/js/core.js
@@ -377,7 +377,8 @@ function renderProducto(p, esLCP = false) {
     <label class="mb-0"><strong>Talle:</strong></label>
     <select id="talle_${p.id_base}" class="form-select form-select-sm w-auto"
             onchange="actualizarStockPorTalle('${p.id_base}', this.value)"
-            style="min-width: 80px; max-width: 160px;">
+            style="min-width: 80px; max-width: 160px;"
+            aria-label="Seleccionar talle para ${p.nombre}">
       <option value="">-</option>
       ${opcionesTalles}
     </select>
